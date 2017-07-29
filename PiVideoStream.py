6de53fecda45d4f1b19a932459d5b4a1291ce563
nick-paul <npaul@ltu.edu>
2017-07-29 20:17:12 +0000

    PiVideoStream prevents repeated frames

--- PiVideoStream.py
+++ PiVideoStream.py
@@ -3,6 +3,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 from threading import Thread
 import cv2
+from Queue import Queue
 
 class PiVideoStream:
     def __init__(self, resolution=(320, 240), framerate=32):
@@ -13,11 +14,12 @@ class PiVideoStream:
         self.rawCapture = PiRGBArray(self.camera, size=resolution)
         self.stream = self.camera.capture_continuous(self.rawCapture,
             format="bgr", use_video_port=True)
-
         # initialize the frame and the variable used to indicate
         # if the thread should be stopped
-        self.frame = None
+        #self.frame = None
         self.stopped = False
+        self.frameready = True
+        self.que = Queue(maxsize=3)
 
     def start(self):
         # start the thread to read frames from the video stream
@@ -29,7 +31,9 @@ class PiVideoStream:
         for f in self.stream:
             # grab the frame from the stream and clear the stream in
             # preparation for the next frame
-            self.frame = f.array
+            self.que.put(f.array)
+            #self.frame = f.array
+            self.frameready = True
             self.rawCapture.truncate(0)
 
             # if the thread indicator variable is set, stop the thread
@@ -39,9 +43,11 @@ class PiVideoStream:
                 self.rawCapture.close()
                 self.camera.close()
                 return
+
     def read(self):
         # return the frame most recently read
-        return self.frame
+        self.frameready = False
+        return self.que.get()
 
     def stop(self):
         # indicate that the thread should be stopped
--- picamera_fps_demo.py
+++ picamera_fps_demo.py
@@ -1,6 +1,6 @@
 # import the necessary packages
 from __future__ import print_function
-from imutils.video.pivideostream import PiVideoStream
+from PiVideoStream import PiVideoStream
 from imutils.video import FPS
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -80,21 +80,27 @@ print("[INFO] sampling THREADED frames from `picamera` module...")
 vs = PiVideoStream().start()
 time.sleep(2.0)
 fps = FPS().start()
+framecount = 0
+vs.debug = True
 
 # loop over some frames...this time using the threaded stream
 while fps._numFrames < args["num_frames"]:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+    # don't process the same frame twice
+    #if not vs.frameready:
+    #    continue
 
-	# check to see if the frame should be displayed to our screen
-	if args["display"] > 0:
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
 
-	# update the FPS counter
-	fps.update()
+    # check to see if the frame should be displayed to our screen
+    if args["display"] > 0:
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+    # update the FPS counter
+    fps.update()
 
 # stop the timer and display FPS information
 fps.stop()
