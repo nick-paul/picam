fdf2b9cd6c66f99dc300131217f8f61cdc89edee
nick-paul <npaul@ltu.edu>
2017-07-29 20:20:04 +0000

    Add picamera and cv combined example
    This example has a low framerate
    PiVideoStream is a better solution

---
+++ picam_cv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# camera warm up
+time.sleep(1)
+
+for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+    image = frame.array
+    
+    cv2.imshow('Frame', image)
+    key = cv2.waitKey(1) & 0xFF
+
+    rawCapture.truncate(0)
+
+    if key == ord('q'):
+        break
+
+
+
+# camera.capture(rawCapture, format='bgr')
+# image = rawCapture.array
+# 
+# cv2.imshow('Image', image)
+# cv2.waitKey(0)
