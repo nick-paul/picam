8d7eef7d53d474ee158a52e6cabd9ecb17a985f1
nick-paul <npaul@ltu.edu>
2017-08-01 19:37:32 +0000

    add gui tests

---
+++ PiVideoStreamGUI.py
@@ -0,0 +1,68 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from threading import Thread
+import cv2
+from Queue import Queue
+import time
+
+class PiVideoStream:
+    def __init__(self, resolution=(320, 240), framerate=32):
+        # initialize the camera and stream
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        
+        # fix camera values
+        iso=100
+        time.sleep(2.0)
+        self.exposure_mode = 'off'
+        self.shutter_speed = 0
+        gains = self.camera.awb_gains
+        self.camera.awb_mode = 'off'
+        self.camera.awb_gains = gains
+
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture,
+            format="bgr", use_video_port=True)
+        # initialize the frame and the variable used to indicate
+        # if the thread should be stopped
+        #self.frame = None
+        self.stopped = False
+        self.frameready = True
+        self.que = Queue(maxsize=3)
+
+        # Camera config
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        for f in self.stream:
+            print(self.camera.iso, self.camera.shutter_speed)
+            # grab the frame from the stream and clear the stream in
+            # preparation for the next frame
+            self.que.put(f.array)
+            #self.frame = f.array
+            self.frameready = True
+            self.rawCapture.truncate(0)
+
+            # if the thread indicator variable is set, stop the thread
+            # and resource camera resources
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
+                return
+
+    def read(self):
+        # return the frame most recently read
+        self.frameready = False
+        return self.que.get()
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
---
+++ pvs_test_gui.py
@@ -0,0 +1,95 @@
+from PiVideoStreamGUI import PiVideoStream
+from picamera import PiCamera
+import cv2
+import time
+
+from Tkinter import *
+
+vs = PiVideoStream()
+
+master = Tk()
+scale_iso = Scale(master,
+        from_=100,
+        to=1600,
+        label='ISO',
+        resolution=100,
+        orient=HORIZONTAL)
+scale_iso.pack()
+
+shutter_speed1 = Scale(master,
+        from_=1,
+        to=30000,
+        label='SS1',
+        resolution=1,
+        orient=HORIZONTAL)
+shutter_speed1.pack()
+
+
+shutter_speed2 = Scale(master,
+        from_=1,
+        to=30000,
+        label='SS2',
+        resolution=1,
+        orient=HORIZONTAL)
+shutter_speed2.pack()
+
+scale_swap_frames = Scale(master,
+        from_=1,
+        to=60,
+        label='swap frames',
+        orient=HORIZONTAL)
+scale_swap_frames.pack()
+
+vs.start()
+
+framecount = 0
+swap_frames = 30
+
+try:
+    while True:
+        framecount += 1
+        frame = vs.read()
+        cv2.imshow('Preview', frame)
+        key = cv2.waitKey(1) & 0xff
+        if key == ord('q'):
+            break
+        
+        master.update()
+        
+        vs.camera.iso = scale_iso.get()
+        
+        swap_frames = scale_swap_frames.get()
+        if framecount % (swap_frames*2) > swap_frames:
+            vs.camera.shutter_speed = shutter_speed1.get()
+        else:
+            vs.camera.shutter_speed = shutter_speed2.get()
+
+
+finally:
+    vs.stop()
+    master.destroy()
+
+
+# camera = PiCamera()
+# camera.resolution = (320, 240)
+# camera.framerate = 32
+
+
+# vs = PiHDRVideoStream(
+#         expseq=[0,600000],
+#         isoseq=[100,800], 
+#         framerate=60).start()
+# #time.sleep(2.0)
+# 
+# while True:
+#     frame = vs.read()
+#     cv2.imshow('Preview', frame)
+#     #print(vs.camera.shutter_speed)
+# 
+#     key = cv2.waitKey(1) & 0xff
+# 
+#     if key == ord('q'):
+#         break
+# 
+# cv2.destroyAllWindows()
+# vs.stop()
