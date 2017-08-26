7a3ad2ad97e9fbe9ca199b46b068b01a5410f04e
nick-paul <npaul@ltu.edu>
2017-08-26 23:43:35 +0000

    add stills

--- hdr_still/Take_hdr.sh
+++ hdr_still/Take_hdr.sh
@@ -5,7 +5,7 @@ TIMEOUT=2000
 NOW=$(date +"%m_%d_%y__%H_%M_%S")
 mkdir -p stills/${NOW}
 
-raspistill -w 320 -h 240 -o stills/${NOW}/std.png --timeout $TIMEOUT
+raspistill -w 640 -h 480 -o stills/${NOW}/std.png --timeout $TIMEOUT
 
 ISO=(100 100 200 400)
 SS=(1 300 2000 15000)
@@ -14,6 +14,6 @@ while [ $COUNTER -lt 4 ];
 do
     ISO_I=${ISO[$COUNTER]}
     SS_I=${SS[$COUNTER]}
-    raspistill -w 320 -h 240 -ISO $ISO_I -ss $SS_I -o stills/${NOW}/cap_iso${ISO_I}_ss_${SS_I}.png --timeout $TIMEOUT
+    raspistill -w 640 -h 480 -ISO $ISO_I -ss $SS_I -o stills/${NOW}/cap_iso${ISO_I}_ss_${SS_I}.png --timeout $TIMEOUT
     let COUNTER=COUNTER+1
 done
