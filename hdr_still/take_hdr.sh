a2c564f92e7577f07e583a00cfc0e15a7f1d57e4
Nicholas Paul <npaul@ltu.edu>
2017-08-27 15:03:31 -0400

    rename file

--- hdr_still/take_hdr.sh
+++ hdr_still/take_hdr.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-TIMEOUT=2000
-
+# Set up variables
+WIDTH=1920
+HEIGHT=1080
+TIMEOUT=1500
 NOW=$(date +"%m_%d_%y__%H_%M_%S")
+
+# Camera parameters
+ISO=(100 100 200 400) # ISO
+SS=(1 300 2000 15000) # Shutter Speed
+
+# Create the directory to store the images
 mkdir -p stills/${NOW}
 
-raspistill -w 320 -h 240 -o stills/${NOW}/std.png --timeout $TIMEOUT
+# Take an autoexposure image
+raspistill -w $WIDTH -h $HEIGHT -o stills/${NOW}/std.png --timeout $TIMEOUT
 
-ISO=(100 100 200 400)
-SS=(1 300 2000 15000)
+# Take manual exposure shots
 COUNTER=0
 while [ $COUNTER -lt 4 ];
 do
     ISO_I=${ISO[$COUNTER]}
     SS_I=${SS[$COUNTER]}
-    raspistill -w 320 -h 240 -ISO $ISO_I -ss $SS_I -o stills/${NOW}/cap_iso${ISO_I}_ss_${SS_I}.png --timeout $TIMEOUT
+    raspistill -w $WIDTH -h $HEIGHT -ISO $ISO_I -ss $SS_I -o stills/${NOW}/cap_iso${ISO_I}_ss_${SS_I}.png --timeout $TIMEOUT
     let COUNTER=COUNTER+1
 done
+
+echo "Done!"
