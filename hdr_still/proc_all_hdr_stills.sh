f4e733c7869e338baf069f9a26cabdd0f1c43ae1
Nicholas Paul <npaul@ltu.edu>
2017-08-27 15:47:58 -0400

    add docs to hdr proc script

--- hdr_still/proc_all_hdr_stills.sh
+++ hdr_still/proc_all_hdr_stills.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# In each directory, create an hdr image
 for dir in $(ls stills);
 do
     dir="stills/$dir"
+
+    # Only create hdr image if the folder doen't
+    # already contain one
     if !(ls $dir | grep -q hdr)
     then
         ./proc_hdr $dir/cap*
     fi
-
 
 done
